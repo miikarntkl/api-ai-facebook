b5b105ce08c0e56415b96fa4b83561238894d36e
miikarntkl <miika.rantakaulio@gmail.com>
2016-11-26 00:24:38 +0200

    test

--- src/app.js
+++ src/app.js
@@ -48,7 +48,7 @@ function processEvent(event) {
         var attachments = event.message.attachments;
         
         if (isDefined(attachments)) {
-            console.log('Attachments defined!');
+            console.log('Attachments defined: ', attachments);
             let x = attachments.type;
             console.log('Attachment type: ', x);
         }
@@ -290,60 +290,6 @@ function isDefined(obj) {
     return obj != null;
 }
 
-const app = express();
-
-app.use(bodyParser.text({type: 'application/json'}));
-
-app.get('/webhook/', (req, res) => {
-    if (req.query['hub.verify_token'] == FB_VERIFY_TOKEN) {
-        res.send(req.query['hub.challenge']);
-
-        setTimeout(() => {
-            doSubscribeRequest();
-        }, 3000);
-    } else {
-        res.send('Error, wrong validation token');
-    }
-});
-
-app.post('/webhook/', (req, res) => {
-    try {
-        var data = JSONbig.parse(req.body);
-
-        if (data.entry) {
-            let entries = data.entry;
-            entries.forEach((entry) => {
-                let messaging_events = entry.messaging;
-                if (messaging_events) {
-                    messaging_events.forEach((event) => {
-                        if (event.message && !event.message.is_echo ||
-                            event.postback && event.postback.payload) {
-                            console.log('Processing event');
-                            processEvent(event);
-                        }
-                    });
-                }
-            });
-        }
-
-        return res.status(200).json({
-            status: "ok"
-        });
-    } catch (err) {
-        return res.status(400).json({
-            status: "error",
-            error: err
-        });
-    }
-
-});
-
-app.listen(REST_PORT, () => {
-    console.log('Rest service ready on port ' + REST_PORT);
-});
-
-doSubscribeRequest();
-
 function formatVenueData(raw) {
     if (!isDefined(raw.response.groups)) {
         return null;
@@ -495,3 +441,58 @@ function findVenue(parameters, callback) {
         callback(null);
     }
 }
+
+
+const app = express();
+
+app.use(bodyParser.text({type: 'application/json'}));
+
+app.get('/webhook/', (req, res) => {
+    if (req.query['hub.verify_token'] == FB_VERIFY_TOKEN) {
+        res.send(req.query['hub.challenge']);
+
+        setTimeout(() => {
+            doSubscribeRequest();
+        }, 3000);
+    } else {
+        res.send('Error, wrong validation token');
+    }
+});
+
+app.post('/webhook/', (req, res) => {
+    try {
+        var data = JSONbig.parse(req.body);
+
+        if (data.entry) {
+            let entries = data.entry;
+            entries.forEach((entry) => {
+                let messaging_events = entry.messaging;
+                if (messaging_events) {
+                    messaging_events.forEach((event) => {
+                        if (event.message && !event.message.is_echo ||
+                            event.postback && event.postback.payload) {
+                            console.log('Processing event');
+                            processEvent(event);
+                        }
+                    });
+                }
+            });
+        }
+
+        return res.status(200).json({
+            status: "ok"
+        });
+    } catch (err) {
+        return res.status(400).json({
+            status: "error",
+            error: err
+        });
+    }
+
+});
+
+app.listen(REST_PORT, () => {
+    console.log('Rest service ready on port ' + REST_PORT);
+});
+
+doSubscribeRequest();
