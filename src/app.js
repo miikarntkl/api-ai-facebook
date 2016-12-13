1c0836a32b86eced87d9c7efc78225d09e15f6d0
miikarntkl <miika.rantakaulio@gmail.com>
2016-12-13 21:10:03 +0200

    Logging parameter values

--- src/app.js
+++ src/app.js
@@ -615,6 +615,8 @@ function formatGETOptions(sender, parameters) {
         }
         options.qs.ll = lat.toString().concat(', ', long.toString());
     } else {
+        console.log('No location');
+        console.log(parameters.toString());
         return null;
     }
 
